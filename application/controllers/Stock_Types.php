d01aae7dccf5b462f6c01ac050654c0395279f5d
Monica Tosino <mdmtosino@gmail.com>
2020-05-17 14:47:47 +0800

    Stock Types styling and access control

--- application/controllers/Stock_Types.php
+++ application/controllers/Stock_Types.php
@@ -12,101 +12,112 @@ class Stock_Types extends CI_Controller {
 	}
 
 	public function index(){
-		if($this->isLoggedIn){
-			$data = array();
-			$data['success_msg'] = $this->session->flashdata('success_msg');
+		if($this->isLoggedIn && $this->session->userdata('status') == $this->config->item('USER_STATUS_ASSOC')['ACTIVE'][0]){
+			if(in_array('STOCK_TYPE', $this->config->item('USER_ROLE_ASSOC_MENU')[$this->session->userdata('user_role')])){
+				$data = array();
+				$data['success_msg'] = $this->session->flashdata('success_msg');
 
-			if($this->session->userdata('success_msg')){
-				$data['success_msg'] = $this->session->userdata('success_msg');
-				$this->session->unset_userdata('success_msg');
-			}
-			if($this->session->userdata('error_msg')){
-				$data['error_msg'] = $this->session->userdata('error_msg');
-				$this->session->unset_userdata('error_msg');
-			}
+				if($this->session->userdata('success_msg')){
+					$data['success_msg'] = $this->session->userdata('success_msg');
+					$this->session->unset_userdata('success_msg');
+				}
+				if($this->session->userdata('error_msg')){
+					$data['error_msg'] = $this->session->userdata('error_msg');
+					$this->session->unset_userdata('error_msg');
+				}
 
-			if($this->input->post('submit_stock_type')){
-				$this->form_validation->set_rules('stock_type', 'Stock Type', 'required|trim');
+				if($this->input->post('submit_stock_type')){
+					$this->form_validation->set_rules('stock_type', 'Stock Type', 'required|trim');
 
-				if($this->form_validation->run() == true){
-					$con = array(
-						'returnType' => 'count',
-						'conditions' => array(
-							'del' => false,
-							'stock_type' => strtoupper($this->input->post('stock_type'))
-						)
-					);
-					$stockTypeCnt = $this->stock_type->getRows($con);
-					if($stockTypeCnt > 0){
-						$data['error_msg'] = 'Stock Type already exists';
+					if($this->form_validation->run() == true){
+						$con = array(
+							'returnType' => 'count',
+							'conditions' => array(
+								'del' => false,
+								'stock_type' => strtoupper($this->input->post('stock_type'))
+							)
+						);
+						$stockTypeCnt = $this->stock_type->getRows($con);
+						if($stockTypeCnt > 0){
+							$data['error_msg'] = 'Stock Type already exists';
+						}else{
+							$stock_type = array(
+								'id'			=> uniqid('', true),
+								'stock_type'	=> strtoupper($this->input->post('stock_type'))
+								);
+							$this->stock_type->insert($stock_type);
+							
+							$this->session->set_flashdata('success_msg', 'Stock Type successfully added!');
+							redirect(current_url());
+						}
+
 					}else{
-						$stock_type = array(
-							'id'			=> uniqid('', true),
-							'stock_type'	=> strtoupper($this->input->post('stock_type'))
-							);
-						$this->stock_type->insert($stock_type);
-						
-						$this->session->set_flashdata('success_msg', 'Stock Type successfully added!');
-						redirect(current_url());
+						$data['error_msg'] = 'Please fill all required fields.';
 					}
 
-				}else{
-					$data['error_msg'] = 'Please fill all required fields.';
+				}else if($this->input->post('submit_delete')){
+					$this->form_validation->set_rules('id', 'Stock Type', 'required|trim');
+
+					if($this->form_validation->run() == true){
+						if($this->stock_type->delete($this->input->post('id'))){
+							echo 'OK';
+							exit();
+						}else{
+							echo 'Could not delete Stock Type. ID does not exist.';
+							exit();
+						}
+					}
 				}
 
-			}else if($this->input->post('submit_delete')){
-				$this->form_validation->set_rules('id', 'Stock Type', 'required|trim');
+				$this->load->view('stock_types/index', $data);
 
-				if($this->form_validation->run() == true){
-					if($this->stock_type->delete($this->input->post('id'))){
-						echo 'OK';
-						exit();
-					}else{
-						echo 'Could not delete Stock Type. ID does not exist.';
-						exit();
-					}					
-				}
+			}else{
+				$this->load->view('components/unauthorized');
 			}
-
-			$this->load->view('stock_types/index', $data);
-
 		}else{
-			redirect('users/login');
+			redirect('users/logout');
 		}
 	}
 
 
 	public function list(){
-		// Datatables Variables
-		$draw = intval($this->input->get("draw"));
-		$start = intval($this->input->get("start"));
-		$length = intval($this->input->get("length"));
+		if($this->isLoggedIn && $this->session->userdata('status') == $this->config->item('USER_STATUS_ASSOC')['ACTIVE'][0] 
+			&& in_array('STOCK_TYPE', $this->config->item('USER_ROLE_ASSOC_MENU')[$this->session->userdata('user_role')])){
+			
+			// Datatables Variables
+			$draw = intval($this->input->get("draw"));
+			$start = intval($this->input->get("start"));
+			$length = intval($this->input->get("length"));
 
-		$con = array(
-			'returnType' => 'list',
-			'conditions' => array(
-				'del' => false
-			)
-		);
-		$list = $this->stock_type->getRows($con);
-
-		$data = array();
-		
-		foreach($list->result_array() as $r) {
-			$data[] = array(
-				$r['ID'],
-			    $r['STOCK_TYPE']
+			$con = array(
+				'returnType' => 'list',
+				'conditions' => array(
+					'del' => false
+				)
 			);
-		}
+			$list = $this->stock_type->getRows($con);
 
-		$output = array(
-		   "draw" => $draw,
-		     "recordsTotal" => $list->num_rows(),
-		     "recordsFiltered" => $list->num_rows(),
-		     "data" => $data
-		);
-		echo json_encode($output);
-		exit();
+			$data = array();
+			
+			foreach($list->result_array() as $r) {
+				$data[] = array(
+					$r['ID'],
+				    $r['STOCK_TYPE']
+				);
+			}
+
+			$output = array(
+			   "draw" => $draw,
+			     "recordsTotal" => $list->num_rows(),
+			     "recordsFiltered" => $list->num_rows(),
+			     "data" => $data
+			);
+			echo json_encode($output);
+			exit();
+
+		}else{
+			$this->load->view('components/unauthorized');
+		}
      }
 
 
