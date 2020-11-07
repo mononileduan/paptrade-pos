f2c65ff6fd2309d3003b98bcc6398feac400c36a
Monica Tosino <mdmtosino@gmail.com>
2020-11-08 01:04:23 +0800

    Sales landscape Print

--- application/views/sales/index.php
+++ application/views/sales/index.php
@@ -284,6 +284,20 @@
 					                },
                						/*autoPrint: false,*/
 	                                customize: function ( win ) {
+	                                	var css = '@page { size: landscape; }',
+						                    head = win.document.head || win.document.getElementsByTagName('head')[0],
+						                    style = win.document.createElement('style');
+						 
+						                style.type = 'text/css';
+						                style.media = 'print';
+						                if (style.styleSheet){
+						                  style.styleSheet.cssText = css;
+						                }else{
+						                  style.appendChild(win.document.createTextNode(css));
+						                }
+						                head.appendChild(style);
+
+
 					                    $(win.document.body)
 					                        .css( 'font-size', '10pt' )
 					                        .prepend(
